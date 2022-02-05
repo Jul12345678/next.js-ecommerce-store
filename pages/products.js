e095d592bccc36ba0fc0d8b07a6cf4a6e5e19535
Julian Schoepf <ferlojulian@hotmail.com>
2022-02-05 14:22:41 +0100

    Animal pages work

--- pages/products.js
+++ pages/products.js
@@ -2,7 +2,7 @@ import { css } from '@emotion/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import productDataBase from '../util/database';
+import productsDataBase from '../util/database';
 
 const productStyles = css`
   border-radius: 5px;
@@ -12,7 +12,6 @@ const productStyles = css`
 `;
 
 export default function Products(props) {
-  console.log('props', JSON.stringify(props, null, 2));
   return (
     <Layout>
       <Head>
@@ -20,15 +19,15 @@ export default function Products(props) {
         <meta description="List of Products" />
       </Head>
       <h1>Products</h1>
-      {productDataBase.props.map((product) => {
+      {props.products.map((product) => {
         return (
-          <div key={`product- ${product.id}`} css={productStyles}>
+          <div key={`product-${product.id}`} css={productStyles}>
             {/* Dynamic link /products/1, etc */}
             <Link href={`/products/${product.id}`}>
               <a>
                 {product.name} is a {product.type}
               </a>
-            </Link>
+            </Link>{' '}
           </div>
         );
       })}
@@ -40,6 +39,6 @@ export default function Products(props) {
 /* Code in getServersideProps only runs in Node.js, and allows you to read files from file system, connect to (real) database, etc.   */
 export function getServerSideProps() {
   return {
-    props: { products: productDataBase },
+    props: { products: productsDataBase },
   };
 }
--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,14 +1,14 @@
 // [fileName] = kind of like a variable / description of information after slash in URL
 import Head from 'next/head';
 import Layout from '../../components/Layout';
-import productDataBase from '../../util/database';
+import productsDataBase from '../../util/database';
 
 export default function SingleProduct(props) {
   return (
     <Layout>
       <Head>
         <title>
-          {props.product.name} ({props.product.type})
+          {props.product.name} ({props.product.type}){' '}
         </title>
         <meta
           description={`${props.product.name} is a ${props.product.type}`}
@@ -18,27 +18,22 @@ export default function SingleProduct(props) {
         {props.product.name} ({props.product.type})
       </h1>
       <div>id: {props.product.id}</div>
-      <div>name: {props.name.id}</div>
-      <div>type: {props.type.id}</div>
+      <div>name: {props.product.name}</div>
+      <div>type: {props.product.type}</div>
     </Layout>
   );
 }
 
 export function getServerSideProps(context) {
   const productId = context.query.productId;
-  console.log('db', productDataBase);
-  const matchingProduct = productDataBase.find((product) => {
-    // eslint-disable-next-line sonarjs/prefer-single-boolean-return
-    if (product.id === productId) {
-      return true;
-    } else {
-      return false;
-    }
+  console.log('db', productsDataBase);
+  const matchingProduct = productsDataBase.find((product) => {
+    return product.id === productId;
   });
 
   return {
     props: {
-      product: matchingProduct[0],
+      product: matchingProduct,
     },
   };
 }
